f5fc9e56f1c756b72632276d9a4c4d9a28fbc8cd
John Glennan <glennan20@gmail.com>
2020-08-25 13:09:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese = "cheddar", "gouda", "camembert"
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.include? {|cheeses| 
     cheese
   else
