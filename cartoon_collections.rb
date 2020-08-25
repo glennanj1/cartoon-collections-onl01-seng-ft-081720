8f40ce92a0304560d6176010f94aea9524cc2c0e
John Glennan <glennan20@gmail.com>
2020-08-25 12:36:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(names)
+  index = 0
+  # print each name in number order 
+  names.each_with_index do |name, index|
+    index += 1
+    puts "#{index} #{name}"
+    
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect { |x| x.capitalize + "!"}
+   
+  
 end
 
 def long_planeteer_calls# code an argument here
