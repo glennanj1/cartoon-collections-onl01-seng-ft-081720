fa714a5dd81927930759be1cb8c092f4ff3965ce
John Glennan <glennan20@gmail.com>
2020-08-25 12:58:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,11 +15,11 @@ def summon_captain_planet(planeteer_calls)
   
 end
 
-def long_planeteer_calls(array) code an argument here
-  if array.length < 4
-    false
-  else
+def long_planeteer_calls(array)
+  if array.length > 4
     true
+  else
+    false
   end
   # Your code here
 end
